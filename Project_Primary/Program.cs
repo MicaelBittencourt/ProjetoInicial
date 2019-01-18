87ccaa2a18c4e8db9a0639ffa69bfd1fe08248c0
Micael Bittencourt <ti.micaelbittencourt@outlook.com>
2019-01-18 11:15:54 -0200

    Foi retirado a frase teste e iniciado um breve questionario, com nome, idade, endereço e email, no final é apresentado todas as informaçoes com a data de cadastro local.

--- Project_Primary/Program.cs
+++ Project_Primary/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,20 @@ namespace Project_Primary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Teste do Git");
+            Console.WriteLine("-----------------Questionário------------------");
+            Console.WriteLine("Qual Seu nome?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Qual sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual Seu endereço?");
+            string endereco = Console.ReadLine();
+            Console.WriteLine("Qual Seu email?");
+            string email = Console.ReadLine();
+            DateTime utcDate = DateTime.UtcNow;
+
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Nome: "+nome+" \nIdade:"+idade+"\nEndereço: "+endereco+"\nData de Cadaastro: "+utcDate.ToLocalTime());
+
         }
     }
 }
